f19671afcaa55429697fb051b5d784edc463fc0b
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-14 12:04:40 -0400

    Update RegistrationController.java

--- src/main/java/com/recipe/server/controller/RegistrationController.java
+++ src/main/java/com/recipe/server/controller/RegistrationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping(path = "/signup")
