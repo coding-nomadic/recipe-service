cc8a134974452c906f3328ffeb15223f50f7a9c2
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-14 11:57:39 -0400

    Update AuthenticationController.java

--- recipe-server/src/main/java/com/recipe/server/controller/AuthenticationController.java
+++ recipe-server/src/main/java/com/recipe/server/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.io.IOException;
 
