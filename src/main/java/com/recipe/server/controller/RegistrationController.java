19e2598b0e224a11cb89277b9f7b0b5b60e52a6c
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-17 12:49:31 -0400

    Update RegistrationController.java

--- src/main/java/com/recipe/server/controller/RegistrationController.java
+++ src/main/java/com/recipe/server/controller/RegistrationController.java
@@ -12,12 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping(path = "/signup")
-@CrossOrigin(origins = "https://recipe-himalayan.netlify.app")
 public class RegistrationController {
 
     @Autowired
