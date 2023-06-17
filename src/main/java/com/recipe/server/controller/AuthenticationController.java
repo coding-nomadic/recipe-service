701332671f604d30ba6fc2f46e8deff94581af94
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-17 12:48:39 -0400

    Update AuthenticationController.java

--- src/main/java/com/recipe/server/controller/AuthenticationController.java
+++ src/main/java/com/recipe/server/controller/AuthenticationController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "https://recipe-himalayan.netlify.app")
 @Slf4j
 public class AuthenticationController {
 
