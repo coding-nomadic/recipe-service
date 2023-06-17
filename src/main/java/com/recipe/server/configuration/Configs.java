89d0c38d294709dce94aa7c7bf770296683c17fa
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-17 12:42:43 -0400

    Update Configs.java

--- src/main/java/com/recipe/server/configuration/Configs.java
+++ src/main/java/com/recipe/server/configuration/Configs.java
@@ -21,17 +21,4 @@ public class Configs {
     public UserService userService() {
         return new UserService();
     }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://boot-hheroku.herokuapp.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-                "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
