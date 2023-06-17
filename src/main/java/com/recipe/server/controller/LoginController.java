a8749138dbaa263081237aad2b32d3f44f6ccbb7
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-17 12:49:10 -0400

    Update LoginController.java

--- src/main/java/com/recipe/server/controller/LoginController.java
+++ src/main/java/com/recipe/server/controller/LoginController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
 @RequestMapping(path = "/api")
-@CrossOrigin(origins = "https://recipe-himalayan.netlify.app")
 public class LoginController {
 
     @Autowired
