a68c47e1de53b36642811080c9cb7a3ed5374c78
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-17 12:48:59 -0400

    Update CommentController.java

--- src/main/java/com/recipe/server/controller/CommentController.java
+++ src/main/java/com/recipe/server/controller/CommentController.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "https://recipe-himalayan.netlify.app")
 public class CommentController {
 
     @Autowired
