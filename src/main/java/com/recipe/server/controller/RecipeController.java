7510aaf3e95c95a73a2d125b80ea1f971aea2dd1
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-18 16:00:33 -0400

    Update RecipeController.java

--- src/main/java/com/recipe/server/controller/RecipeController.java
+++ src/main/java/com/recipe/server/controller/RecipeController.java
@@ -9,11 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RequestMapping(value = "/api/v1/recipes")
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RecipeController {
     @Autowired
     private RecipeService recipeService;
