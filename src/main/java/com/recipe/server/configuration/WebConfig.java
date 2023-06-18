5a049f26f84859dc3a8ce2a0c0d629cf6d0f8f39
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-18 16:00:16 -0400

    Update WebConfig.java

--- src/main/java/com/recipe/server/configuration/WebConfig.java
+++ src/main/java/com/recipe/server/configuration/WebConfig.java
@@ -1,20 +1,20 @@
-// package com.recipe.server.configuration;
+package com.recipe.server.configuration;
 
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// @Configuration
-// @EnableWebMvc
-// public class WebConfig implements WebMvcConfigurer {
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 .allowedOrigins("*") // Allow requests from any origin
-//                 .allowedMethods("GET", "POST","PUT","DELETE") // Allow all HTTP methods
-//                 .allowedHeaders("*") 
-//                 .allowCredentials(true)
-//                 .maxAge(3600);// Allow all headers
-//     }
-// }
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("*") // Allow all HTTP methods
+                .allowedHeaders("*") 
+                .allowCredentials(true)
+                .maxAge(3600);// Allow all headers
+    }
+}
