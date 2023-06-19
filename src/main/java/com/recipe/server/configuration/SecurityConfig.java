240165397d23c3179435ca41fa21f07600060455
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-18 20:29:42 -0400

    Update SecurityConfig.java

--- src/main/java/com/recipe/server/configuration/SecurityConfig.java
+++ src/main/java/com/recipe/server/configuration/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter ;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer ;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebSecurity
