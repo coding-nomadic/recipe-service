24490c7d7cd0d35c0315168e52df05bde826207c
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-18 20:25:42 -0400

    Update WebConfig.java

--- src/main/java/com/recipe/server/configuration/WebConfig.java
+++ src/main/java/com/recipe/server/configuration/WebConfig.java
@@ -9,13 +9,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
-                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                        "Access-Control-Request-Headers","Authorization")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true).maxAge(3600);
-    }
-
-}
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*");
+  }
+} 
