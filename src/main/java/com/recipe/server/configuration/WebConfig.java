e39ec52ef5e34178b05d66e85d60628556632b9a
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-18 20:28:48 -0400

    Update WebConfig.java

--- src/main/java/com/recipe/server/configuration/WebConfig.java
+++ src/main/java/com/recipe/server/configuration/WebConfig.java
@@ -1,16 +1,16 @@
-package com.recipe.server.configuration;
+// package com.recipe.server.configuration;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter ;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter ;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig extends WebMvcConfigurerAdapter {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*");
-  }
-} 
+//   @Override
+//   public void addCorsMappings(CorsRegistry registry) {
+//     registry.addMapping("/**").allowedOrigins("*");
+//   }
+// } 
