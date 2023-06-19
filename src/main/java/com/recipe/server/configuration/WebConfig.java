a1a0404128d43b072822bbf8922238708bcb9b39
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-18 20:22:32 -0400

    Update WebConfig.java

--- src/main/java/com/recipe/server/configuration/WebConfig.java
+++ src/main/java/com/recipe/server/configuration/WebConfig.java
@@ -3,7 +3,7 @@ package com.recipe.server.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter ;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
