c810d4c74059d4387746a76077390db5197be32b
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-20 10:15:44 -0400

    Update CategoryService.java

--- src/main/java/com/recipe/server/service/CategoryService.java
+++ src/main/java/com/recipe/server/service/CategoryService.java
@@ -7,6 +7,8 @@ import com.recipe.server.models.CategoryResponse;
 import com.recipe.server.repository.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,13 +23,13 @@ public class CategoryService {
     CategoryRepository categoryRepository;
     @Autowired
     ModelMapper modelMapper;
-
+    @CacheEvict(value = "categories", allEntries = true)
     public CategoryResponse saveCategory(CategoryRequest categoryRequest) {
         Category category = modelMapper.map(categoryRequest, Category.class);
         Category categoryResponse = categoryRepository.save(category);
         return modelMapper.map(categoryResponse, CategoryResponse.class);
     }
-
+    @CacheEvict(value = "categories", allEntries = true)
     public CategoryResponse updateCategory(CategoryRequest categoryRequest, Long id) {
         Category category = categoryRepository.findById(id).orElseThrow(
                                         () -> new ResourceNotFoundException("Category Id not found", "102"));
@@ -36,19 +38,19 @@ public class CategoryService {
         Category categoryResponse = categoryRepository.save(category);
         return modelMapper.map(categoryResponse, CategoryResponse.class);
     }
-
+    @Cacheable("categories")
     public CategoryRequest getCategoryById(Long id) {
         Category category = categoryRepository.findById(id).orElseThrow(
                                         () -> new ResourceNotFoundException("Category ID not found", "102"));
         return modelMapper.map(category, CategoryRequest.class);
     }
-
+    @Cacheable("categories")
     public List<CategoryRequest> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
         return categories.stream().map(category -> modelMapper.map(category, CategoryRequest.class))
                                         .collect(Collectors.toList());
     }
-
+    @CacheEvict(value = "categories", allEntries = true)
     public void deleteById(Long id) {
         categoryRepository.deleteById(id);
     }
