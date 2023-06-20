d9cdfa4a7cdf3216c57cc4a01ff84a6be8b41beb
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-20 10:54:24 -0400

    Update RecipeServerApplication.java

--- src/main/java/com/recipe/server/RecipeServerApplication.java
+++ src/main/java/com/recipe/server/RecipeServerApplication.java
@@ -1,9 +1,12 @@
 package com.recipe.server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableCaching
 public class RecipeServerApplication {
 
 	public static void main(String[] args) {
