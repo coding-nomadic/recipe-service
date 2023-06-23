8173f8dad0227b73f70284b558ae05fc4f1d2a9c
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-23 17:25:36 -0400

    Update Configs.java

--- src/main/java/com/recipe/server/configuration/Configs.java
+++ src/main/java/com/recipe/server/configuration/Configs.java
@@ -30,7 +30,7 @@ public class Configs {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("https://recipe-service-ixhm.onrender.com"));
+        config.setAllowedOrigins("*");
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
