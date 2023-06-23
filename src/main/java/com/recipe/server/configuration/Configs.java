632cfa55babf398724b87320ea92de065febf257
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-23 16:59:23 -0400

    Update Configs.java

--- src/main/java/com/recipe/server/configuration/Configs.java
+++ src/main/java/com/recipe/server/configuration/Configs.java
@@ -2,9 +2,16 @@ package com.recipe.server.configuration;
 
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class Configs {
@@ -18,5 +25,16 @@ public class Configs {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("https://recipe-service-ixhm.onrender.com"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean registration = new FilterRegistrationBean(new CorsFilter(source));
+        return registration;
+    }
 }
