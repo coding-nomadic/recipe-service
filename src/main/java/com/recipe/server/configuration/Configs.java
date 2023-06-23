e6efb1830b5250e04068a328b44e18ceca88af5e
Tenzin Dawa <tnzngdw@gmail.com>
2023-06-23 18:35:33 -0400

    Update Configs.java

--- src/main/java/com/recipe/server/configuration/Configs.java
+++ src/main/java/com/recipe/server/configuration/Configs.java
@@ -1,23 +1,17 @@
 package com.recipe.server.configuration;
 
-
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import java.util.Arrays;
-import java.util.Collections;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +26,13 @@ public class Configs extends WebSecurityConfigurerAdapter{
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-     @Bean
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.csrf().disable().authorizeRequests().antMatchers("/authenticate","/authenticate/**", "/signup", "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs", "/webjars/**", "/api/login", "/api/logout", "/signup/confirm").permitAll().anyRequest().authenticated().and().exceptionHandling().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors();
+        // http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
