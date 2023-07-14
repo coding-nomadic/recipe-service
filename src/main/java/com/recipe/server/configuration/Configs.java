8bcefad9d5ee7fc0b6914814f184f11f5eb31a0b
Tenzin Dawa <tnzngdw@gmail.com>
2023-07-14 14:18:54 -0400

    Update Configs.java

--- src/main/java/com/recipe/server/configuration/Configs.java
+++ src/main/java/com/recipe/server/configuration/Configs.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,23 +13,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class Configs extends WebSecurityConfigurerAdapter{
+public class Configs extends WebSecurityConfigurerAdapter {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //http.csrf().disable().authorizeRequests().antMatchers("/authenticate","/authenticate/**", "/signup", "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs", "/webjars/**", "/api/login", "/api/logout", "/signup/confirm").permitAll().anyRequest().authenticated().and().exceptionHandling().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors();
-        // http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
