0135ce1667df08c16a8e00d84e6c74d5dfb5e55f
Tenzin Dawa <tnzngdw@gmail.com>
2023-07-14 17:17:42 -0400

    Update CorsFilter.java

--- src/main/java/com/recipe/server/configuration/CorsFilter.java
+++ src/main/java/com/recipe/server/configuration/CorsFilter.java
@@ -1,43 +1,43 @@
 
-package com.recipe.server.configuration;
+// package com.recipe.server.configuration;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+// import lombok.extern.slf4j.Slf4j;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+// import javax.servlet.*;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import java.io.IOException;
 
-@Component
-@Slf4j
-public class CorsFilter implements Filter {
+// @Component
+// @Slf4j
+// public class CorsFilter implements Filter {
 
 
 
-    public CorsFilter() {
-        log.info("CORSFilter init");
-    }
+//     public CorsFilter() {
+//         log.info("CORSFilter init");
+//     }
 
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
-        chain.doFilter(req, res);
-    }
+//     @Override
+//     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//         HttpServletRequest request = (HttpServletRequest) req;
+//         HttpServletResponse response = (HttpServletResponse) res;
+//         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+//         response.setHeader("Access-Control-Allow-Credentials", "true");
+//         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//         response.setHeader("Access-Control-Max-Age", "3600");
+//         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+//         chain.doFilter(req, res);
+//     }
 
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
+//     @Override
+//     public void init(FilterConfig filterConfig) {
+//     }
 
-    @Override
-    public void destroy() {
-    }
-}
+//     @Override
+//     public void destroy() {
+//     }
+// }
