370488e35b5fe003de31e237538e352028a992b1
Tenzin Dawa <tnzngdw@gmail.com>
2023-09-07 21:21:09 -0400

    Update AnalyticsController.java

--- src/main/java/com/recipe/server/controller/AnalyticsController.java
+++ src/main/java/com/recipe/server/controller/AnalyticsController.java
@@ -1,9 +1,7 @@
 package com.recipe.server.controller;
 
-
 import com.recipe.server.entity.Recipe;
 import com.recipe.server.service.AnalyticsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping(value = "/api/v1/analytics")
 @RestController
+@RequestMapping(value = "/api/v1/analytics")
 @CrossOrigin(origins = "*")
 public class AnalyticsController {
 
-    @Autowired
-    private AnalyticsService analyticsService;
+    private final AnalyticsService analyticsService;
 
-    /**
-     * @return
-     */
+    public AnalyticsController(AnalyticsService analyticsService) {
+        this.analyticsService = analyticsService;
+    }
+
     @GetMapping(path = "/popularRecipes")
     public List<Recipe> getPopularRecipes() {
         return analyticsService.generatePopularRecipesReport();
