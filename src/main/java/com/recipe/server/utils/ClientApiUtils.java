dc157bf57ae64b2f7b732c06cd6122cf893c553d
Tenzin Dawa <tnzngdw@gmail.com>
2023-09-07 21:30:19 -0400

    Update ClientApiUtils.java

--- src/main/java/com/recipe/server/utils/ClientApiUtils.java
+++ src/main/java/com/recipe/server/utils/ClientApiUtils.java
@@ -1,22 +1,28 @@
-package com.recipe.server.utils;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ClientApiUtils {
+public final class ClientApiUtils {
 
-    ClientApiUtils(){}
-      /**
-     * 
-     * @param url
-     * @return
-     * @throws Exception
+    private ClientApiUtils() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Calls an external API using HTTP GET.
+     *
+     * @param url The URL of the external API.
+     * @return The HTTP response from the external API.
+     * @throws Exception If an error occurs during the API call.
      */
     public static HttpResponse<String> callExternalApi(String url) throws Exception {
         HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .build();
+
         return httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
     }
 }
