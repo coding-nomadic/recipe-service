d596e2e82bb0a6b3dc8da7ef73121cfca8a77120
Tenzin Dawa <tnzngdw@gmail.com>
2023-09-07 21:28:02 -0400

    Update CommentService.java

--- src/main/java/com/recipe/server/service/CommentService.java
+++ src/main/java/com/recipe/server/service/CommentService.java
@@ -1,6 +1,5 @@
 package com.recipe.server.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.recipe.server.entity.Comment;
 import com.recipe.server.entity.Recipe;
@@ -17,61 +16,81 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @Transactional
 public class CommentService {
 
-    private static final String POST_NOT_FOUND = "Recipe not found";
+    private static final String RECIPE_NOT_FOUND = "Recipe not found";
     private static final String COMMENT_NOT_FOUND = "Comment not found";
     private static final String COMMENT_NOT_BELONG = "Comment does not belong to Recipe";
+
     @Autowired
     private ModelMapper mapper;
 
     @Autowired
-    RecipeRepository postRepository;
+    private RecipeRepository recipeRepository;
 
     @Autowired
-    CommentRepository commentRepository;
+    private CommentRepository commentRepository;
 
     @CacheEvict(value = "comments", allEntries = true)
-    public CommentResponse createComment(long postId, CommentRequest commentRequest) {
+    public CommentResponse createComment(long recipeId, CommentRequest commentRequest) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new ResourceNotFoundException(RECIPE_NOT_FOUND, "102"));
+
         Comment comment = mapper.map(commentRequest, Comment.class);
-        Recipe post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Recipe ID not found", "102"));
-        comment.setRecipe(post);
-        Comment commentResponse = commentRepository.save(comment);
-        return mapper.map(commentResponse, CommentResponse.class);
+        comment.setRecipe(recipe);
+
+        Comment savedComment = commentRepository.save(comment);
+        return mapper.map(savedComment, CommentResponse.class);
     }
 
     @Cacheable("comments")
-    public CommentRequest getCommentById(Long postId, Long commentId) {
-        Recipe post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException(POST_NOT_FOUND, "102"));
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException(POST_NOT_FOUND, "102"));
-        if (!comment.getRecipe().getId().equals(post.getId())) {
-            throw new RecipeServiceException(COMMENT_NOT_BELONG, "102");
-        }
+    public CommentRequest getCommentById(Long recipeId, Long commentId) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new ResourceNotFoundException(RECIPE_NOT_FOUND, "102"));
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResourceNotFoundException(COMMENT_NOT_FOUND, "102"));
+
+        validateCommentBelongs(recipe, comment);
+
         return mapper.map(comment, CommentRequest.class);
     }
 
     @CacheEvict(value = "comments", allEntries = true)
-    public CommentResponse updateComment(Long postId, long commentId, CommentRequest commentRequest) {
-        Recipe post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException(POST_NOT_FOUND, "102"));
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException(COMMENT_NOT_FOUND, "id"));
-        if (!comment.getRecipe().getId().equals(post.getId())) {
-            throw new RecipeServiceException(COMMENT_NOT_BELONG, "102");
-        }
+    public CommentResponse updateComment(Long recipeId, long commentId, CommentRequest commentRequest) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new ResourceNotFoundException(RECIPE_NOT_FOUND, "102"));
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResourceNotFoundException(COMMENT_NOT_FOUND, "id"));
+
+        validateCommentBelongs(recipe, comment);
+
         comment.setBody(commentRequest.getBody());
         comment.setRatings(commentRequest.getRatings());
-        Comment commentResponse = commentRepository.save(comment);
-        return mapper.map(commentResponse, CommentResponse.class);
+
+        Comment updatedComment = commentRepository.save(comment);
+        return mapper.map(updatedComment, CommentResponse.class);
     }
+
     @CacheEvict(value = "comments", allEntries = true)
-    public void deleteComment(Long postId, Long commentId) {
-        Recipe post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException(POST_NOT_FOUND, "id"));
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException(COMMENT_NOT_FOUND, "id"));
-        if (!comment.getRecipe().getId().equals(post.getId())) {
+    public void deleteComment(Long recipeId, Long commentId) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new ResourceNotFoundException(RECIPE_NOT_FOUND, "id"));
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResourceNotFoundException(COMMENT_NOT_FOUND, "id"));
+
+        validateCommentBelongs(recipe, comment);
+
+        commentRepository.delete(comment);
+    }
+
+    private void validateCommentBelongs(Recipe recipe, Comment comment) {
+        if (!comment.getRecipe().getId().equals(recipe.getId())) {
             throw new RecipeServiceException(COMMENT_NOT_BELONG, "102");
         }
-        commentRepository.delete(comment);
     }
 }
