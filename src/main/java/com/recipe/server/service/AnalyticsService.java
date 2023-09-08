7ebd3477a21996b3fd22e84f85b7202ab6598c1d
Tenzin Dawa <tnzngdw@gmail.com>
2023-09-07 21:26:30 -0400

    Update AnalyticsService.java

--- src/main/java/com/recipe/server/service/AnalyticsService.java
+++ src/main/java/com/recipe/server/service/AnalyticsService.java
@@ -4,26 +4,56 @@ import com.recipe.server.entity.Recipe;
 import com.recipe.server.repository.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 @Slf4j
 public class AnalyticsService {
+
+    private final RecipeRepository recipeRepository;
+
     @Autowired
-    private RecipeRepository recipeRepository;
+    public AnalyticsService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     public List<Recipe> generatePopularRecipesReport() {
-        int maxCommentCount = recipeRepository.findAll().stream().mapToInt(s -> s.getComments().size()).summaryStatistics().getMax();
-        log.info("Maximum count from the recipe records : " + maxCommentCount);
-        return recipeRepository.findAll().stream().filter(s -> s.getComments().size() == maxCommentCount).collect(Collectors.toList());
+        Optional<Integer> maxCommentCount = recipeRepository.findAll()
+                .stream()
+                .map(r -> r.getComments().size())
+                .max(Integer::compareTo);
+        
+        if (maxCommentCount.isPresent()) {
+            log.info("Maximum count from the recipe records: " + maxCommentCount.get());
+            return recipeRepository.findAll()
+                    .stream()
+                    .filter(r -> r.getComments().size() == maxCommentCount.get())
+                    .collect(Collectors.toList());
+        } else {
+            log.warn("No recipes found");
+            return List.of();
+        }
     }
 
     public List<Recipe> mostCookTimeRecipes() {
-        int maxCookTime = recipeRepository.findAll().stream().mapToInt(i -> Integer.valueOf(i.getCookTime())).summaryStatistics().getMax();
-        log.info("Maximum cook Time from the recipe records : " + maxCookTime);
-        return recipeRepository.findAll().stream().filter(s -> Integer.valueOf(s.getCookTime()) == maxCookTime).collect(Collectors.toList());
+        Optional<Integer> maxCookTime = recipeRepository.findAll()
+                .stream()
+                .map(r -> Integer.parseInt(r.getCookTime()))
+                .max(Integer::compareTo);
+
+        if (maxCookTime.isPresent()) {
+            log.info("Maximum cook time from the recipe records: " + maxCookTime.get());
+            return recipeRepository.findAll()
+                    .stream()
+                    .filter(r -> Integer.parseInt(r.getCookTime()) == maxCookTime.get())
+                    .collect(Collectors.toList());
+        } else {
+            log.warn("No recipes found");
+            return List.of();
+        }
     }
 }
