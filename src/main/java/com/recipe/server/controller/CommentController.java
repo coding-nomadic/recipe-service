7bc90725a4a46ca7d8f83e670f56a3e52c554557
Tenzin Dawa <tnzngdw@gmail.com>
2023-10-24 16:12:02 -0400

    Update CommentController.java

--- src/main/java/com/recipe/server/controller/CommentController.java
+++ src/main/java/com/recipe/server/controller/CommentController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1")
